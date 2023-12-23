ca6f80c90783079245af8e875b1763d1ccfbb09d
c_sanjay <gowriganeshns@saveetha.ac.in>
2023-12-24 00:18:24 +0530

    success

--- companywebsite/companywebsite/settings.py
+++ companywebsite/companywebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!^&0uvezr#*umls^r&q99r_bvh*%)*p9vgy&f7uvs=h@s-@uy_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
